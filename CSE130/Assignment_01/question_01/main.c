2b63570e55f8df227902ebed88bfdca0cbd04e76
Saad Ahmad <sashuvo379@gmail.com>
2026-06-20 21:27:55 +0600

    Saad\ISRT-32\Asign_01\Ques_01

---
+++ CSE130/Assignment_01/question_01/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+
+    printf("\n+----------------------------------+\n");
+    printf("|         ISRT - CSE 130           |\n");
+    printf("|     Programming with C/C++       |\n");
+    printf("+----------------------------------+\n");
+    printf("|  Name: _ _ _ _ _ _ _ _ _ _       |\n");
+    printf("|  Roll: _ _ _ _ _ _ _ _ _ _       |\n");
+    printf("+----------------------------------+\n\n");
+
+
+    return 0;
+}
+
